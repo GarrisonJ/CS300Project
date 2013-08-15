efd50a4311d10b62d64b5c0bac0bb5251b5ecf07
Garrison Jensen <garrison.jensen@gmail.com>
2013-08-15 12:50:29 -0700

    Added map New() function

--- cs_300_ampla_project/Map.vb
+++ cs_300_ampla_project/Map.vb
@@ -1,6 +1,13 @@
 ﻿Public Class Map
     Dim Cell_Array(,) As Cell
+    Dim Graph_Window As Graphics
+    Dim Graph_Rectangle As Rectangle
 
+    Sub New(ByRef Cell_Map_x As Integer, ByRef Cell_Map_y As Integer, ByRef WindowGraph As Graphics, ByRef Rectangle_for_map As Rectangle)
+        Dim MyMap(Cell_Map_x, Cell_Map_y) As Cell
+        Graph_Window = WindowGraph
+        Graph_Rectangle = Rectangle_for_map
+    End Sub
 
     Sub Draw()
 
