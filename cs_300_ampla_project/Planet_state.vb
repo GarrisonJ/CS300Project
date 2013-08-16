8e96149ce9000e76512041cf7bfe5c1d98db74b9
Garrison Jensen <garrison.jensen@gmail.com>
2013-08-16 14:47:37 -0700

    added display functions.

--- cs_300_ampla_project/Planet_state.vb
+++ cs_300_ampla_project/Planet_state.vb
@@ -1,5 +1,16 @@
-﻿
-Public Class Planet_state
+﻿Public Class Planet_state
     Dim initial_state As Map
     Dim current_state As Map
+
+
+    Sub Display_current_state(ByRef graphic_to_display_graph As Graphics, ByRef rectange_window As Rectangle)
+
+    End Sub
+
+    Sub Display_initial_state(ByRef graphic_to_display_graph As Graphics, ByRef rectange_window As Rectangle)
+
+    End Sub
+
+
+
 End Class
