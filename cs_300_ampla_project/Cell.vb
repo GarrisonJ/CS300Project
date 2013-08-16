cbd16dc953fb5d33052917f5651796dc17d7a0c9
Garrison Jensen <garrison.jensen@gmail.com>
2013-08-16 15:37:35 -0700

    Cell set color function.

--- cs_300_ampla_project/Cell.vb
+++ cs_300_ampla_project/Cell.vb
@@ -1,8 +1,14 @@
 ﻿Public Class Cell
-    Dim Cell_Color As Color
+    Dim Cell_color As Color
     Dim Cell_pen As Pen
     Dim SideLength As Integer
     Dim Location As Point
+
+
+    Sub Set_cell_color(ByRef new_cell_color As Color)
+        Cell_color = new_cell_color
+        Cell_pen.Color = Cell_color
+    End Sub
 
     Sub Draw_cell(ByRef graphic_contex As Graphics)
         Dim rec As Rectangle = New Rectangle(Location, New Size(Location))
