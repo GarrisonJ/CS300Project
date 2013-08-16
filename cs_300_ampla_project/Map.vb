b0e2230ee8997d12da14f2eca4ce15a8b4711c3b
Garrison Jensen <garrison.jensen@gmail.com>
2013-08-16 15:26:28 -0700

    Made a list of cells.

--- cs_300_ampla_project/Map.vb
+++ cs_300_ampla_project/Map.vb
@@ -3,9 +3,21 @@
     Dim Graph_Window As Graphics
     Dim Graph_Rectangle As Rectangle
     Dim Map_state As State
+    Dim list_of_cells As List(Of Cell)
+    Dim num_of_cells As Integer
 
     Sub New(ByRef Cell_Map_X_dim As Integer, ByRef Cell_Map_Y_dim As Integer, ByRef initial_state As State)
         Dim Cell_Array(Cell_Map_X_dim, Cell_Map_Y_dim) As Cell
+        num_of_cells = Cell_Map_X_dim * Cell_Map_Y_dim
+        Dim i As Integer
+        Dim j As Integer
+        While i < Cell_Map_X_dim
+            While j < Cell_Map_Y_dim
+                list_of_cells.Add(Cell_Array(i, j))
+                j = j + 1
+            End While
+            i = i + 1
+        End While
         Map_state = initial_state
     End Sub
 
