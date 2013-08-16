543c4df81bd1d12876a28b29742ce11c887bfd75
Garrison Jensen <garrison.jensen@gmail.com>
2013-08-16 15:35:26 -0700

    draw cell function.

--- cs_300_ampla_project/Cell.vb
+++ cs_300_ampla_project/Cell.vb
@@ -1,5 +1,11 @@
 ﻿Public Class Cell
     Dim Cell_Color As Color
+    Dim Cell_pen As Pen
     Dim SideLength As Integer
     Dim Location As Point
+
+    Sub Draw_cell(ByRef graphic_contex As Graphics)
+        Dim rec As Rectangle = New Rectangle(Location, New Size(Location))
+        graphic_contex.DrawRectangle(Cell_pen, rec)
+    End Sub
 End Class
