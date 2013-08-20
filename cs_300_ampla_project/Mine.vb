ba03aff2b651378cf9206c92d17a6f498b004f69
Garrison Jensen <garrison.jensen@gmail.com>
2013-08-19 17:11:36 -0700

    Get and Set toxicity functions.

--- cs_300_ampla_project/Mine.vb
+++ cs_300_ampla_project/Mine.vb
@@ -1,3 +1,13 @@
 ﻿Public Class Mine
     Dim Toxicity As Integer
+
+
+    Sub set_toxicity(ByVal toxicity_level As Integer)
+        Me.Toxicity = toxicity_level
+    End Sub
+
+    Function set_toxicity() As Integer
+        Return Me.Toxicity
+    End Function
+
 End Class
