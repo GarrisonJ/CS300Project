6b981023d3c58f35fa863b7cf0dd267a21beba8d
Garrison Jensen <garrison.jensen@gmail.com>
2013-08-20 10:31:30 -0700

    Changed toxicity from int to double.

--- cs_300_ampla_project/Mine.vb
+++ cs_300_ampla_project/Mine.vb
@@ -1,9 +1,9 @@
 ﻿Public Class Mine
     Inherits Cell
 
-    Dim Toxicity As Integer
+    Dim Toxicity As Double
 
-    Sub set_toxicity(ByVal toxicity_level As Integer)
+    Sub set_toxicity(ByVal toxicity_level As Double)
         Me.Toxicity = toxicity_level
     End Sub
 
