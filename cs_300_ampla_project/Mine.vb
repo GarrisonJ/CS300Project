c9aaaa249a79e752fa559881ef41d419b78964a1
Garrison Jensen <garrison.jensen@gmail.com>
2013-08-20 19:24:29 -0700

    Added comments to mine class.

--- cs_300_ampla_project/Mine.vb
+++ cs_300_ampla_project/Mine.vb
@@ -1,12 +1,21 @@
-﻿Public Class Mine
+﻿' Copyright (c) 2013 Garrison Jensen <garrison.jensen@gmail.com>
+
+' A mine is a cell with a toxicity level
+Public Class Mine
     Inherits Cell
 
-    Dim Toxicity As Double
+    Dim Toxicity As Double ' Toxicity level of cell
 
+    ' Set toxicity level.
+    ' INPUT:
+    '   toxicity_level - Toxicity level of mine
     Sub set_toxicity(ByVal toxicity_level As Double)
         Me.Toxicity = toxicity_level
     End Sub
 
+    ' Get toxicity level.
+    ' OUTPUT:
+    '   Double - Toxicity level
     Function get_toxicity() As Integer
         Return Me.Toxicity
     End Function
