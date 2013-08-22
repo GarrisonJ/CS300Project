28455be5ac6c1e164869bf1a5c27b1a7e68b9bdf
Garrison Jensen <garrison.jensen@gmail.com>
2013-08-22 16:55:16 -0700

    Cell should use a brush not a pen.

--- cs_300_ampla_project/Cell.vb
+++ cs_300_ampla_project/Cell.vb
@@ -1,9 +1,9 @@
 ﻿' Copyright (c) 2013 Garrison Jensen <garrison.jensen@gmail.com>
 Public Class Cell
-    Dim Cell_color As Color     ' Cell color
-    Dim Cell_pen As Pen         ' Cell pen, should be same color as cell color
-    Dim SideLength As Integer   ' Cell is a square, this is its side length
-    Dim Location As Point       ' Location of cell
+    Dim Cell_color As Color      ' Cell color
+    Dim Cell_Brush As SolidBrush ' Cell pen, should be same color as cell color
+    Dim SideLength As Integer    ' Cell is a square, this is its side length
+    Dim Location As Point        ' Location of cell
 
     ' Set side length
     ' INPUT:
@@ -23,11 +23,17 @@ Public Class Cell
     ' INPUT:
     '   new_cell_color - New color for cell.
     Sub Set_cell_color(ByRef new_cell_color As Color)
-        Cell_color = new_cell_color
-        If IsNothing(Cell_pen) Then
-            Cell_pen = New Pen(Brushes.White)
+
+        If IsNothing(Cell_color) Then
+            Cell_color = New Color
         End If
-        Cell_pen.Color = Cell_color
+        Cell_color = new_cell_color
+
+        If IsNothing(Cell_Brush) Then
+            Cell_Brush = New SolidBrush(Cell_color)
+        End If
+        Cell_Brush.Color = Cell_color
+
     End Sub
 
     ' Set cell point.
@@ -43,10 +49,9 @@ Public Class Cell
     ' INPUT:
     '   graphic_contex - Graphic contex for pixel to draw itself.
     Sub Draw_cell(ByRef graphic_contex As Graphics)
-        If Not IsNothing(Location) And Not IsNothing(SideLength) And Not IsNothing(Cell_pen) Then
+        If Not IsNothing(Location) And Not IsNothing(SideLength) And Not IsNothing(Cell_Brush) Then
             Dim rec As Rectangle = New Rectangle(SideLength * Location.X, SideLength * Location.Y, SideLength, SideLength)
-            Dim new_brush As New SolidBrush(Cell_color)
-            graphic_contex.FillRectangle(new_brush, rec)
+            graphic_contex.FillRectangle(Cell_Brush, rec)
         End If
     End Sub
 
